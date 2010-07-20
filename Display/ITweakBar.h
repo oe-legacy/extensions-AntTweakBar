9003da51261e94683ef84add271374e776053a26
ptx <ptx@openengine.dk>
2010-07-20 15:10:06 +0200

    Added parameters for positoon and iconify
    Ignore-this: d6270ead7b7842546a1a3672cf0b43d4
    
    darcs-hash:20100720131006-2a962-3b1453d6584663718df3ca6cd4b276c393b3de48.gz

--- Display/ITweakBar.h
+++ Display/ITweakBar.h
@@ -3,6 +3,9 @@
 
 #include <string>
 #include <AntTweakBar/include/AntTweakBar.h>
+#include <Math/Vector.h>
+#include <Logging/Logger.h>
+#include <stdint.h>
 
 using namespace std;
 
@@ -34,20 +37,34 @@ namespace Display {
 //     }
 // };
 
+using namespace Math;
+
 class AntTweakBar;
 
 class ITweakBar {
 private:
     string name;
     TwBar *bar;
+
+    bool iconify;
+    Vector<2,float> position;
+    
 public:
-    ITweakBar(string name) : name(name) {};
+    ITweakBar(string name) 
+  : name(name)
+  , bar(0)
+  , iconify(true)
+    {};
     virtual ~ITweakBar() {};
 
     void SetupBar(AntTweakBar&m) {
         bar = TwNewBar(name.c_str());
 
         AddFields(m);
+        
+        _SetIconify();
+        _SetPosition();
+
     };
 
     TwBar* GetBar() {return bar;}
@@ -56,6 +73,27 @@ public:
 
     virtual void AddFields(AntTweakBar&m) =0;
     
+    void _SetIconify() {
+        int32_t val = iconify;
+        TwSetParam(bar, NULL, "iconified", TW_PARAM_INT32, 1,  &val);
+    }
+    void _SetPosition() {
+        ostringstream oss;
+        oss << position[0] << ' ' << position[1];
+        TwSetParam(bar, NULL, "position",TW_PARAM_CSTRING, 1, oss.str().c_str());
+    }
+
+    void SetIconify(bool s) {
+        iconify = s;
+        if (bar) _SetIconify(); 
+    }
+
+    void SetPosition(Vector<2,float> p) {
+        position = p; 
+        if (bar) _SetPosition();
+    }
+
+
     // static void TW_CALL AntCallback(void* cp) {
     //     ICallback* cb = (ICallback*)cp;
     //     cb->Call();
